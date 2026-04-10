41005ced044a829b91d049337634edc56f5761a7
Ilikecarpet <onilmamo@gmail.com>
2026-04-10 16:14:27 +1200

    feat: update MainPanel to render workspace pane trees

--- ui/src/components/MainPanel/MainPanel.tsx
+++ ui/src/components/MainPanel/MainPanel.tsx
@@ -1,35 +1,41 @@
-import { useTerminalStore } from "../../stores/terminalStore";
+import { useWorkspaceStore } from "../../stores/workspaceStore";
 import { useGitStore } from "../../stores/gitStore";
 import { useServiceStore } from "../../stores/serviceStore";
-import TabBar from "./TabBar";
-import TerminalView from "./TerminalView";
+import WorkspaceBar from "./WorkspaceBar";
+import PaneView from "./PaneView";
 import LogOverlay from "./LogOverlay";
 import GitPanel from "./GitPanel";
 import styles from "./MainPanel.module.css";
 
 export default function MainPanel() {
-  const tabs = useTerminalStore((s) => s.tabs);
-  const activeTabId = useTerminalStore((s) => s.activeTabId);
+  const workspaces = useWorkspaceStore((s) => s.workspaces);
+  const activeWorkspaceId = useWorkspaceStore((s) => s.activeWorkspaceId);
   const activeGitGroupId = useGitStore((s) => s.activeGitGroupId);
   const activeLogSvcId = useServiceStore((s) => s.activeLogSvcId);
 
+  const activeWs = workspaces.find((w) => w.id === activeWorkspaceId);
   const showEmpty =
-    tabs.length === 0 && !activeGitGroupId && !activeLogSvcId;
-  const showGitPanel = activeGitGroupId && !activeTabId;
+    workspaces.length === 0 && !activeGitGroupId && !activeLogSvcId;
+  const showGitPanel = activeGitGroupId && !activeWorkspaceId;
 
   return (
     <div className={styles.main}>
-      <TabBar />
+      <WorkspaceBar />
       <div className={styles.termArea}>
         {showEmpty && (
           <div className={styles.emptyState}>
-            Press + to open a terminal
+            Press + to open a workspace
           </div>
         )}
 
-        {tabs.map((tab) => (
-          <TerminalView key={tab.id} tabId={tab.id} />
-        ))}
+        {activeWs && (
+          <div style={{ position: "absolute", inset: 0 }}>
+            <PaneView
+              node={activeWs.root}
+              activePaneId={activeWs.activePaneId}
+            />
+          </div>
+        )}
 
         {showGitPanel && <GitPanel />}
 
